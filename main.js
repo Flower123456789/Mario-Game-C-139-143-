7f6a8d88ac15a8bd68276236c13e41d7980f0b95
Flower123456789 <82616100+Flower123456789@users.noreply.github.com>
2022-01-09 14:32:49 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+
 function preload() {
+
 	world_start = loadSound("world_start.wav");
 	setSprites();
 	MarioAnimation();
@@ -7,8 +9,25 @@ function preload() {
 function setup() {
 	canvas = createCanvas(1240,336);
     canvas.parent('canvas');
+
 	instializeInSetup(mario);
+
+	video = createCapture(VIDEO)
+	video.size(800, 350);
+	video.parent('game_console');
+	
 }
+
+
+function gotPoses(results) {
+	if(results.length > 0) {
+		console.log(results);
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+	}
+}
+
+
 
 function draw() {
 	game()
