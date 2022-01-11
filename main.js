03c867a1e4240ce1bfa9ef60481e70fdb98951f4
Flower123456789 <82616100+Flower123456789@users.noreply.github.com>
2022-01-11 16:28:08 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+ noseX = 0;
+ noseY = 0;
 
+ 
 function preload() {
 
 	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_gameover = loadSound("gameover.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
 	setSprites();
 	MarioAnimation();
 }
